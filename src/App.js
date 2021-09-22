bd07687e6e3811d55b981b619d08539dffef8206
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2021-09-22 22:20:14 +0400

    Leader board

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ function App() {
     //give data in this API
     //https://starnavi-frontend-test-task.herokuapp.com/game-settings
     const url = "https://starnavi-frontend-test-task.herokuapp.com/game-settings";
+    const winnersUrl = "https://starnavi-frontend-test-task.herokuapp.com/winners";
 
     const [gameModes, setGameModes] = useState({});
     const [selectedMode, setSelectedMode] = useState("")
     const [selectedName, setSelectedName] = useState("")
     const [selectedData, setSelectedData] = useState({})
+    const [winners, setWinners] = useState([])
 
+    //get mode type
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(res => setGameModes(res))
+    }, [])
+
+    //get winners data
+    useEffect(() => {
+        fetch(winnersUrl)
+            .then(res => res.json())
+            .then(res => setWinners(res))
     }, [])
 
     const handleChangeMode = (event) => {
@@ -51,34 +61,38 @@ function App() {
                 row[i].push(<p style={myStyle.p} key={`${i}-${j}`}>{`${i}-${j}`}</p>);
             }
         }
+        // console.log("Row ", row);
         return row;
     };
 
     return (
         <div className="App">
-            <Container>
+            <Container fluid={true}>
                 <Row>
                     <Col>
                         <h1 className="text-center my-3">Game In Dots</h1>
                     </Col>
                 </Row>
                 <Row>
-                    <Col lg="8" md="12" className="App__Game_left">
+                    <Col lg="6" md="12" className="App__Game_left">
                         <form onSubmit={handleSubmit}>
                             <label>
-                                <select value={selectedMode} onChange={handleChangeMode}>
+                                <select value={selectedMode} onChange={handleChangeMode}
+                                        className="App__Game_left_dropdown">
                                     <option value="">Pick game mode</option>
                                     {Object.entries(gameModes).map((gameMode, idx) => (
-                                        <option key={idx} value={gameMode[0]}>{gameMode[0]}</option>
+                                        <option key={idx} value={gameMode[0]}>
+                                            {gameMode[0] === "easyMode" ? "Easy" : gameMode[0] === "normalMode" ? "Normal" : "Hard"}
+                                        </option>
                                     ))}
                                 </select>
                             </label>
                             <label>
-                                <input type="text" value={selectedName} onChange={handleChangeName}/>
+                                <input type="text" value={selectedName} onChange={handleChangeName}
+                                       className="App__Game_left_name" placeholder="Enter your name"/>
                             </label>
-                            <input type="submit" value="PLAY" disabled={!selectedMode}/>
+                            <input type="submit" value="PLAY" disabled={!selectedMode} className="App__Game_left_btn"/>
                         </form>
-                        {console.log(selectedData)}
                         <h3 className="text-center my-3">Welcome to the Game In Dots</h3>
 
                         <div className="App__Game_left_table" style={myStyle.div}>
@@ -87,7 +101,25 @@ function App() {
                             }
                         </div>
                     </Col>
-                    <Col lg="4" md="12" className="App__Game_right">
+                    <Col lg="6" md="12" className="App__Game_right">
+                        <h2>Leader Board</h2>
+                        <table className="App__Game_right_table">
+                            <thead>
+                            <tr>
+                                <th>User Name</th>
+                                <th>Date and Time</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {winners && winners.map(winner => (
+                                <tr key={winner.id}>
+                                    <td>{winner.winner}</td>
+                                    <td>{winner.date}</td>
+                                </tr>
+
+                            ))}
+                            </tbody>
+                        </table>
                     </Col>
                 </Row>
             </Container>
